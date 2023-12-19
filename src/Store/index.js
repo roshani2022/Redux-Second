74846337f3757cb7464cdefc4ac2630301c36e96
roshani2022 <rosh.gupta17@gmail.com>
2023-12-19 23:26:36 +0530

    Auth Reducer

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,12 @@
 
 import { createSlice,configureStore } from "@reduxjs/toolkit";
 
-const initialState =  { counter: 0, showCounter:true }
+const initialCounterState =  { counter: 0, showCounter:true }
+const initialAuthenticationState = {isAuthenticated:false}
 
 const counterSlice = createSlice({
     name:'counter',
-    initialState,
+    initialState:initialCounterState,
     reducers:{
         increase(state){
             state.counter++
@@ -22,11 +23,25 @@ const counterSlice = createSlice({
     }
 })
 
+const authSlice = createSlice({
+    name:'authentication',
+    initialState:initialAuthenticationState,
+    reducers:{
+      login(state){
+         state.isAuthenticated=true
+      },
+      logout(state){
+        state.isAuthenticated=false
+      }
+    }
+})
+
 
 const store = configureStore({
-    reducer:counterSlice.reducer
+    reducer:{counter:counterSlice.reducer,auth:authSlice.reducer}
 });
 
 export const counterAction = counterSlice.actions
+export const authAction = authSlice.actions
 
 export default store;
